ae97b4294ab4a1d772d2ef9e742695a6478496e1
dongminjung <dongminjung@Dongmins-MacBook-Pro.local>
2020-10-22 23:54:34 +0900

    update

--- tests/testthat/test_ttgsea.R
+++ tests/testthat/test_ttgsea.R
@@ -12,39 +12,65 @@ embedding_dims <- 50
 num_units <- 32
 epochs <- 1
 
-ttgseaRes <- fit_model(fgseaRes, "pathway", "NES",
-                       model = bi_lstm(num_tokens, embedding_dims,
-                                       length_seq, num_units),
-                       num_tokens = num_tokens,
-                       length_seq = length_seq,
-                       epochs = epochs,
-                       batch_size = batch_size)
+
+if (reticulate::py_available() & keras::is_keras_available()) {
+  ttgseaRes <- fit_model(fgseaRes, "pathway", "NES",
+                         model = bi_lstm(num_tokens, embedding_dims,
+                                         length_seq, num_units),
+                         num_tokens = num_tokens,
+                         length_seq = length_seq,
+                         epochs = epochs,
+                         batch_size = batch_size)
+}
 
 
+
+check_python_available <- function() {
+  if (!reticulate::py_available()) {
+    skip("Python is not available on this system")
+  }
+}
+
+
+check_keras_available <- function() {
+  if (!keras::is_keras_available()) {
+    skip("Keras is not available on this system")
+  }
+}
 
 
 
 test_that("fit_model: the output of fit_model is list", {
+  check_python_available()
+  check_keras_available()
   expect_type(ttgseaRes, "list")
 })
 
 
 test_that("fit_model: fit_model yields a model", {
+  check_python_available()
+  check_keras_available()
   expect_type(ttgseaRes$model, "closure")
 })
 
 
 test_that("fit_model: num_tokens in the model", {
+  check_python_available()
+  check_keras_available()
   expect_equal(ttgseaRes$num_tokens, num_tokens)
 })
 
 
 test_that("fit_model: length_seq in the model", {
+  check_python_available()
+  check_keras_available()
   expect_equal(ttgseaRes$length_seq, length_seq)
 })
 
 
 test_that("fit_model: swap values of text and score", {
+  check_python_available()
+  check_keras_available()
   expect_error(fit_model(fgseaRes, "NES", "pathway",
                          model = bi_lstm(num_tokens, embedding_dims,
                                          length_seq, num_units),
@@ -56,6 +82,8 @@ test_that("fit_model: swap values of text and score", {
 
 
 test_that("fit_model: miss model", {
+  check_python_available()
+  check_keras_available()
   expect_error(fit_model(fgseaRes, "pathway", "NES",
                          num_tokens = num_tokens,
                          length_seq = length_seq,
@@ -65,6 +93,8 @@ test_that("fit_model: miss model", {
 
 
 test_that("fit_model: miss epochs", {
+  check_python_available()
+  check_keras_available()
   expect_error(fit_model(fgseaRes, "pathway", "NES",
                          model = bi_lstm(num_tokens, embedding_dims,
                                          length_seq, num_units),
@@ -75,6 +105,8 @@ test_that("fit_model: miss epochs", {
 
 
 test_that("fit_model: miss batch_size", {
+  check_python_available()
+  check_keras_available()
   expect_error(fit_model(fgseaRes, "pathway", "NES",
                          model = bi_lstm(num_tokens, embedding_dims,
                                          length_seq, num_units),
@@ -86,27 +118,37 @@ test_that("fit_model: miss batch_size", {
 
 
 test_that("predict_model: the output of predict_model is list", {
+  check_python_available()
+  check_keras_available()
   expect_type(predict_model(ttgseaRes, "Cell Cycle"), "list")
 })
 
 
 test_that("predict_model: the number of rows of the result is the length of new_text", {
+  check_python_available()
+  check_keras_available()
   expect_equal(nrow(predict_model(ttgseaRes, c("Cell Cycle", "DNA Replication"))), 2)
 })
 
 
 test_that("predict_model: miss new_text", {
+  check_python_available()
+  check_keras_available()
   expect_error(predict_model(ttgseaRes))
 })
 
 
 
 test_that("plot_model: output", {
+  check_python_available()
+  check_keras_available()
   expect_visible(plot_model(ttgseaRes$model))
 })
 
 
 test_that("plot_model: only model is required", {
+  check_python_available()
+  check_keras_available()
   expect_error(plot_model(ttgseaRes))
 })
 
